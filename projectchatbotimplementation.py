76a708999a533edd4988ceb84966d64092605c55
Godfrey Purification <111069009+GodfreyPurification@users.noreply.github.com>
2025-11-05 21:21:41 +0600

    Initial commit: Chatbot Python project

---
+++ projectchatbotimplementation.py
@@ -0,0 +1,58 @@
+from audioop import reverse
+import json
+import pickle
+import random
+from sys import intern
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+import numpy as np
+
+from chatbot import intents, lemmatizer
+
+model=load_model('chatbotmodel.h5')
+intern=json.loads(open('intents.json').read())
+words=pickle.load(open('words.pkl', 'rb'))
+classes=pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words=nltk.word_tokenize(sentence)
+    sentence_words=[lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bag_of_word(sentence, words, show_details=True):
+    sentence_words=clean_up_sentence(sentence)
+    bag=[0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w==s: 
+                bag[i] = 1 
+                if show_details: 
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
+def predict_class(sentence,model): 
+    p=bag_of_word(sentence,words,show_details=False)
+    res=model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD=0.25
+    results=[[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x:x[1], reverse=True)
+    return_list=[]
+    for r in results: 
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list 
+def get_response(ints, intents_json): 
+    tag=ints[0]['intent']
+    list_of_intents=intents_json['intents']
+    for i in list_of_intents: 
+        if (i['tag']==tag): 
+            result=random.choice(i['responses'])
+            break
+    return result
+
+print("Start chatting with the bot!")
+while True: 
+    message=input("")
+    ints=predict_class(message,model)
+    res=get_response(ints, intents)
+    print(res)
